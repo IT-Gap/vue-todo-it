952d6a54a71691e794103284fb08ed0a99cfc502
b3hniya <b3hniya@gmail.com>
2021-06-04 10:30:08 +0100

    Components made and added to project.

---
+++ src/components/MainCard.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "MainCard"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoCounter.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "TodoCounter"
+}
+</script>
+
+<style scoped>
+
+</style>
